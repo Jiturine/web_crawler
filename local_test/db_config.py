a343a140188375776ce0971d18dd48ea5ed03c7a
wwwwwwwq <32955796+wwwwwwwq@users.noreply.github.com>
2025-05-19 10:54:32 +0800

    Add files via upload

---
+++ local_test/db_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pymysql
+
+DB_CONFIG = {
+    'host': 'localhost',#服务器IP地址
+    'port': 3306,#端口
+    'user': 'root',#数据库用户名
+    'password': '123456',#数据库密码
+    'database': 'web_crawler',#数据库名
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor
+} 
