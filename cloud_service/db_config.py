d98439bfcaefd9766dd9da2b745a8d91119907a7
wwwwwwwq <32955796+wwwwwwwq@users.noreply.github.com>
2025-05-19 13:42:25 +0800

    Update db_config.py

--- cloud_service/db_config.py
+++ cloud_service/db_config.py
@@ -4,8 +4,8 @@
 import pymysql
 
 DB_CONFIG = {
-    'host': '106.52.139.242',#服务器IP地址
-    'port': 8000,#端口
+    'host': '106.52.136.242',#服务器IP地址
+    'port': 3306,#端口
     'user': 'root',#数据库用户名
     'password': '123456',#数据库密码
     'database': 'web_crawler',#数据库名
